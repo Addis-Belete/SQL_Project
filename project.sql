263185812d049ded8cb992e8e23f126219bffc08
Addis Belete <addis.bel@gmail.com>
2021-04-06 11:49:36 +0300

    Add answer for select nobel

--- project.sql
+++ project.sql
@@ -25,7 +25,8 @@ SELECT name, population, area FROM world
 WHERE area > 3000000 OR population > 250000000
 
 SELECT name, population, area FROM world
-WHERE area > 3000000 AND population < 250000000 OR area < 3000000 AND population > 250000000
+WHERE area > 3000000 AND population < 250000000 OR area < 3000000 
+AND population > 250000000
 
 SELECT name, ROUND(population/1000000, 2), ROUND(GDP/1000000000, 2) FROM world
 WHERE continent = 'South America'
@@ -41,4 +42,57 @@ WHERE LEFT(name, 1) = LEFT(capital, 1) AND name <> capital
 
 SELECT name
    FROM world
-WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name NOT LIKE '% %' AND name LIKE '%o%' AND name LIKE '%u%'
+WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%'
+ AND name NOT LIKE '% %' AND name LIKE '%o%' AND name LIKE '%u%'
+
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
+
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+
+SELECT yr, Subject FROM nobel
+WHERE winner = 'Albert Einstein'
+
+SELECT winner FROM nobel
+WHERE yr >= 2000 AND subject = 'Peace'
+
+SELECT yr,subject, winner FROM nobel
+WHERE yr BETWEEN 1980 AND 1989 AND subject ='Literature'
+
+SELECT * FROM nobel
+ WHERE 
+   winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter',
+                  'Barack Obama')
+
+SELECT winner FROM nobel
+WHERE winner LIKE 'John%'
+
+SELECT * FROM nobel
+WHERE (yr = 1980 AND Subject ='Physics' OR yr = 1984 AND subject = 'Chemistry')
+
+SELECT * FROM nobel
+WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine')
+
+SELECT * FROM nobel
+WHERE yr < 1910 AND subject = 'Medicine' OR yr >= 2004 AND subject = 'Literature'
+
+SELECT * FROM nobel
+WHERE winner LIKE 'PETER GRÜNBERG'
+
+SELECT * FROM nobel
+WHERE winner LIKE 'EUGENE O_NEILL'
+
+SELECT winner, yr, subject FROM nobel
+WHERE winner LIKE 'sir%' 
+ORDER BY yr DESC 
+
+SELECT winner,subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY subject IN ('Physics','Chemistry'),subject,winner
